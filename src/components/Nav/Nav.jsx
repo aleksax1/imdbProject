ffd810a6976726961c52e1f7d280b785f7f223ae
merisacuric01 <121257527+merisacuric01@users.noreply.github.com>
2025-01-14 19:59:01 +0100

    izmene unete

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,15 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import "./Nav.css";
-
+import { NavLink } from "react-router-dom";
+import "./Nav.css"; 
 function Nav() {
   return (
-    <nav>
-      
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/movie-details">MovieDetails</Link>
-      <Link to="/all-movies">All Movies</Link>
+    <nav className="navbar">  
+        <h1 className="logo-text" style={{backgroundColor:"#7AB2D3"}}>IMDb</h1>
+
+    
+      <div className="nav-links">
+        <NavLink to="/" activeClassName="active-link" className="nav-link">
+          Home
+        </NavLink>
+        <NavLink to="/about" activeClassName="active-link" className="nav-link">
+          About
+        </NavLink>
+        <NavLink to="/movie-details" activeClassName="active-link" className="nav-link">
+          Movie Details
+        </NavLink>
+        <NavLink to="/all-movies" activeClassName="active-link" className="nav-link">
+          All Movies
+        </NavLink>
+      </div>
+
+      <div className="search-bar">
+     
+        <input type="text" placeholder="Search..." className="search-input" /> 
+      </div>
     </nav>
   );
 }
