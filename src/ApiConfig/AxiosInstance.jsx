4fb8f6543497f5022b13380385732f052f042197
MaidHalilovic <maidhaliloviclimunada567@gmail.com>
2025-01-16 19:19:04 +0100

    AllMovies page is done

--- src/ApiConfig/AxiosInstance.jsx
+++ src/ApiConfig/AxiosInstance.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: "https://imdb-top-100-movies.p.rapidapi.com/",
   headers: {
-    "x-rapidapi-key": "0d173c76c2msh0cab6bcaa89f2ccp18f8f4jsn338a476a3b90",
+    "x-rapidapi-key": "bb43337385mshe68b3e9733bd7dfp1c32efjsn908d2511a448",
     "x-rapidapi-host": "imdb-top-100-movies.p.rapidapi.com",
   },
 });
